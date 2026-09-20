0da76f4d4b470b8e6175de152bcfebdd805f5a03
toptrojan <dmitrykargalev@gmail.com>
2026-09-20 13:49:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const JSONBIN_KEY = "$2a$10$m16rSI18.BeEGuPiI2Dqn.nRhGfcuVkPHh3Bl3GrkLsm6G38saHyS";   
 export const JSONBIN_BIN_ID = "6aafacf1ffd5d160531bb4cb";            
-export const ADMIN_NICK = "trjnnntop12344040t13admindddsalam";
+export const ADMIN_NICK = "trojanadminpanel322233838383888884444ffffffffeeeeeettetetetetette";
