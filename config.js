485c7c1ffc2a29b00d18b84605243de946412094
toptrojan <dmitrykargalev@gmail.com>
2026-09-20 12:50:06 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+
+export const JSONBIN_KEY = "$2b$10$ВАШ_MASTER_KEY";   // замените
+export const JSONBIN_BIN_ID = "ВАШ_BIN_ID";            // создайте bin ниже
+export const ADMIN_NICK = "trjnnntop12344040t13admindddsalam";
