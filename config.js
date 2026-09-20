19d8dde689573de66d10924937d3133a9c83148a
toptrojan <dmitrykargalev@gmail.com>
2026-09-20 15:42:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
 window.APP_CONFIG = {
-  OWNER: "toptrojan",
-  ADMIN_EMAIL: "mymymyeee@outlook.com",
-  WEB3FORMS_KEY: "852ae57a-d624-431f-900c-6baf31e3717d",
   JSONBIN_KEY: "$2a$10$m16rSI18.BeEGuPiI2Dqn.nRhGfcuVkPHh3Bl3GrkLsm6G38saHyS",
   JSONBIN_BIN_ID: "6aafacf1ffd5d160531bb4cb"
 };
