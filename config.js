3ca66acd1129816e76832e1b5a801d51a26961a0
toptrojan <dmitrykargalev@gmail.com>
2026-09-20 14:01:02 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-export const JSONBIN_KEY = "$2a$10$m16rSI18.BeEGuPiI2Dqn.nRhGfcuVkPHh3Bl3GrkLsm6G38saHyS";   
-export const JSONBIN_BIN_ID = "6aafacf1ffd5d160531bb4cb";            
-export const ADMIN_NICK = "eflmpfelMmlpefgjopeokefkfepekfep4235355535gggfgfggg456455454545";
