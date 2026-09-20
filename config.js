4177a2205fd6c1cacef3c70c7369f9c4fff09df4
toptrojan <dmitrykargalev@gmail.com>
2026-09-20 14:36:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 export const OWNER = "toptrojan";
-
 export const GITHUB_CLIENT_ID = "Ov23liCYyZVGbNIrIwrb";
-
-export const JSONBIN_KEY    = "$2a$10$m16rSI18.BeEGuPiI2Dqn.nRhGfcuVkPHh3Bl3GrkLsm6G38saHyS";
+export const JSONBIN_KEY = "$2a$10$m16rSI18.BeEGuPiI2Dqn.nRhGfcuVkPHh3Bl3GrkLsm6G38saHyS";
 export const JSONBIN_BIN_ID = "6aafacf1ffd5d160531bb4cb";
